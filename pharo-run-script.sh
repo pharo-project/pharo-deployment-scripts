ab139bcf220577b32a6653b08dfbd3987c25778e
DEMAREY Christophe <christophe.demarey@inria.fr>
2015-06-16 17:22:32 +0200

    formatting

--- pharo-run-script.sh
+++ pharo-run-script.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 function usage() {
-	cat <<END
+    cat <<END
 Usage: $0
-	run a Smalltalk image
+    run a Smalltalk image
 Naming
-	<app_name> 			is the name of your application and is used as unique identifier (script name as default value)
-	<image>.image 	is the Smalltalk image that will be started
-	<st_file> 			must exist and is the Smalltalk startup script
-	<vm> 				is the full path to the Pharo virtual machine
-	<vm_options> 		[optional] parameters that will be passed to the vm
-	<user>				user used to run the application
+    <app_name>      is the name of your application and is used as unique identifier (script name as default value)
+    <image>.image   is the Smalltalk image that will be started
+    <st_file>       must exist and is the Smalltalk startup script
+    <vm>            is the full path to the Pharo virtual machine
+    <vm_options>    [optional] parameters that will be passed to the vm
+    <user>          user used to run the application
 END
-	exit 1
+    exit 1
 }
 
 if [ "$#" -ne 0 ]; then
-	usage
+    usage
 fi
 
 ### Please update these values accordingly to your server configuration
@@ -35,13 +35,13 @@ echo Executing $0
 echo Working directory $app_home
 
 if [ ! -f "$image" ]; then
-	echo $image not found
-	exit 1
+    echo $image not found
+    exit 1
 fi
 
 if [ ! -f "$st_file" ]; then
-	echo $st_file not found
-	exit 1
+    echo $st_file not found
+    exit 1
 fi
 
 echo Starting $app_name
